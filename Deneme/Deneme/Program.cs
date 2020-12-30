9381fa2ad4a0c97d10a54851bf44a12f4225df13
fatih <fatihatamturk@gmail.com>
2020-12-30 19:16:45 +0300

    İlk yükleme

---
+++ Deneme/Deneme/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bu bir deneme projesidir.");
+        }
+    }
+}
